512aa51ac56486fb9756a63b109956c6c51d61cd
Rohit Kapoor <rohit6301@gmail.com>
2021-03-06 14:47:10 +0000

    Move Home to a component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Navbar from "./components/navbar";
+import Home from "./components/home";
 
 class App extends Component {
   render() {
     return (
-      <Navbar></Navbar>
-    );
+      <>
+        <Navbar />
+        <Home />
+      </>
+    )
   }
 }
 
---
+++ src/components/home.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+    render() {
+        return (
+            <section className="hero-wrap js-fullheight" style={{ height: '1129px' }}>
+                <div className="overlay" />
+                <div className="container">
+                    <div className="row no-gutters slider-text js-fullheight justify-content-center align-items-center" style={{ height: '1129px' }}>
+                        <div className="col-lg-8 col-md-6 ftco-animate d-flex align-items-center fadeInUp ftco-animated">
+                            <div className="text text-center">
+                                <span className="subheading">Hey! I am</span>
+                                <h1>Rohit Kapoor</h1>
+                                <h2>I'm a <span className="txt-rotate" data-period={2000} data-rotate="[ &quot;Software Engineer.&quot;, &quot;Photographer.&quot;, &quot;Gamer.&quot;, &quot;Tech Enthusiast.&quot;, &quot;Traveler.&quot; ]"><span className="wrap" /></span>
+                                </h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="mouse">
+                    <a href="#" className="mouse-icon">
+                        <div className="mouse-wheel"><span className="ion-ios-arrow-round-down" /></div>
+                    </a>
+                </div>
+            </section>
+        )
+    }
+}
