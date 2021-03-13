650de339178c7189d0e2af385ab03cf3d4e7c419
Rohit Kapoor <rohit6301@gmail.com>
2021-03-13 14:58:58 +0000

    Updates

--- js/main.js
+++ js/main.js
@@ -11,17 +11,6 @@
 		scrollProperty: 'scroll'
 	});
 
-
-	var fullHeight = function () {
-
-		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function () {
-			$('.js-fullheight').css('height', $(window).height());
-		});
-
-	};
-	fullHeight();
-
 	// loader
 	var loader = function () {
 		setTimeout(function () {
@@ -167,71 +156,6 @@
 		});
 	};
 	scrollWindow();
-
-
-
-	var counter = function () {
-
-		$('#section-counter, .hero-wrap, .ftco-counter, .ftco-about').waypoint(function (direction) {
-
-			if (direction === 'down' && !$(this.element).hasClass('ftco-animated')) {
-
-				var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
-				$('.number').each(function () {
-					var $this = $(this),
-						num = $this.data('number');
-					console.log(num);
-					$this.animateNumber(
-						{
-							number: num,
-							numberStep: comma_separator_number_step
-						}, 7000
-					);
-				});
-
-			}
-
-		}, { offset: '95%' });
-
-	}
-	counter();
-
-
-	var contentWayPoint = function () {
-		var i = 0;
-		$('.ftco-animate').waypoint(function (direction) {
-
-			if (direction === 'down' && !$(this.element).hasClass('ftco-animated')) {
-
-				i++;
-
-				$(this.element).addClass('item-animate');
-				setTimeout(function () {
-
-					$('body .ftco-animate.item-animate').each(function (k) {
-						var el = $(this);
-						setTimeout(function () {
-							var effect = el.data('animate-effect');
-							if (effect === 'fadeIn') {
-								el.addClass('fadeIn ftco-animated');
-							} else if (effect === 'fadeInLeft') {
-								el.addClass('fadeInLeft ftco-animated');
-							} else if (effect === 'fadeInRight') {
-								el.addClass('fadeInRight ftco-animated');
-							} else {
-								el.addClass('fadeInUp ftco-animated');
-							}
-							el.removeClass('item-animate');
-						}, k * 50, 'easeInOutExpo');
-					});
-
-				}, 100);
-
-			}
-
-		}, { offset: '95%' });
-	};
-	contentWayPoint();
 
 	// magnific popup
 	$('.image-popup').magnificPopup({
